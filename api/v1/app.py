4459e05e423cb8ec3cc3f9aa18281ec911f22467
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-01-25 15:21:59 -0800

    api

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""app variable"""
+
+from flask import Flask, make_response, render_template
+from models import storage
+from api.v1.views import app_views
+from flask import Blueprint, jsonify
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(e):
+    storage.close()
+
+
+@app.errorhandler(404)
+def notfound(e):
+    return make_response({'error': 'Not found'}, 404)
+
+
+if __name__ == "__main__":
+    app.run(host=getenv('HBNB_API_HOST', default='0.0.0.0'),
+            port=getenv('HBNB_API_PORT', default=5000), threaded=True)
