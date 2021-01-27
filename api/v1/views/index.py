c041f12260167deecc727337f33b4f037c3dd14e
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-01-27 14:13:11 -0800

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,19 @@ from models import storage
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    a = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+        }
+    b = {}
+    for k, v in a.items():
+        b[v] = storage.count(k)
+    return jsonify(b)
+
