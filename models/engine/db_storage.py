0bbf7cabe41f2281a5941c4e4718228537e2d86d
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-01-27 08:00:16 -0800

    added_methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,20 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID,
+        or None if not found"""
+        if cls is not None or id is not None:
+            for o in self.all(cls).values():
+                if o.id == id:
+                    return o
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
+        o = self.all(cls)
+        return len(o)
