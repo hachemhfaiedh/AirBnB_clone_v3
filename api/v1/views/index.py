04ab782834cc119350d47920eb8474c3ebd3883d
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-01-27 08:50:10 -0800

    code_fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,16 +2,16 @@
 """API status"""
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify
 from models import storage
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False )
 def stats():
     a = {
         "Amenity": "amenities",
